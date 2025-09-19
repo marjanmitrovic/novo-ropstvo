3bf3eb03ddbcf1c357d6a7dd607c837c70d91be6
novo-ropstvo <marjan.posao@gmail.com>
2025-09-19 07:47:23 +0200

    Ažuriranje sajta 2025-09-19 07:47:23

---
+++ config.js
@@ -0,0 +1,2 @@
+// config.js — Apps Script URL (već upisan)
+window.NR_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzKBoAMr48twIo_QZN746rbbOCLnJasfLzN76wRuuCw84RSRgjc7K6A9udgXkCEbdJidw/exec";
---
+++ js/config.js
@@ -0,0 +1,2 @@
+// config.js — automatski postavljen URL za Apps Script Web App
+window.NR_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzKBoAMr48twIo_QZN746rbbOCLnJasfLzN76wRuuCw84RSRgjc7K6A9udgXkCEbdJidw/exec";
